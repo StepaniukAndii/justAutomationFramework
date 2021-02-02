74d2957e63b7ccb9f57d582205bd02b3e1db3729
Sam <grinred69@gmail.com>
2021-02-02 17:12:28 +0200

    my first time

---
+++ src/test/java/TestFacebook.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+
+public class TestFacebook extends TestInit{
+    @Test
+        public void loginFacebook(){
+        TestFacebook testFacebook = new TestFacebook()
+
+
+    }
+
+
+}
