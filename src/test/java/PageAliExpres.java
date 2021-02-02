62b753af5fcd16465f469628b3de3d08e51a559c
Zoom <zoomfo@gmail.com>
2021-02-02 16:34:14 +0300

    add TestAli and PageTestAli

---
+++ src/test/java/PageAliExpres.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class PageAliExpres extends BasePage{
+
+    public PageAliExpres(WebDriver driver) {
+        super (driver);
+    }
+
+    public WebElement serchFild(){
+        return driver.findElement (By.xpath ("//input[@id='search-form__input']"));
+    }
+    public WebElement closeSpam(){
+        return driver.findElement (By.xpath ("//div[@id='cookies-banner__container__close-btn']"));
+    }
+
+}
