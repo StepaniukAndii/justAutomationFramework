19a6fee45e905935a894a9d96a42614021435617
Zoom <zoomfo@gmail.com>
2021-02-02 16:55:05 +0300

    fixed mistakes testAli and pageAli

--- src/test/java/PageAliExpres.java
+++ src/test/java/PageAliExpres.java
@@ -2,16 +2,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class PageAliExpres extends BasePage{
+public class PageAliExpres extends BasePage {
 
     public PageAliExpres(WebDriver driver) {
         super (driver);
     }
 
-    public WebElement serchFild(){
+    public WebElement serchFild() {
         return driver.findElement (By.xpath ("//input[@id='search-form__input']"));
     }
-    public WebElement closeSpam(){
+
+    public WebElement closeSpam() {
         return driver.findElement (By.xpath ("//div[@id='cookies-banner__container__close-btn']"));
     }
 
--- src/test/java/TestAliExpres.java
+++ src/test/java/TestAliExpres.java
@@ -1,14 +1,14 @@
 import org.testng.annotations.Test;
 
-public class TestAliExpres extends TestInit{
+public class TestAliExpres extends TestInit {
 
     @Test
 
-    public void startTest(){
-         PageAliExpres pageAliExpres =new PageAliExpres (driver);
-         driver.get ("https://allo.ua/ru/kompjutery/");
-         pageAliExpres.serchFild().click ();
-         pageAliExpres.serchFild().sendKeys ("холодильник");
+    public void startTest() {
+        PageAliExpres pageAliExpres = new PageAliExpres (driver);
+        driver.get ("https://allo.ua/ru/kompjutery/");
+        pageAliExpres.serchFild ().click ();
+        pageAliExpres.serchFild ().sendKeys ("холодильник");
     }
 
 }
