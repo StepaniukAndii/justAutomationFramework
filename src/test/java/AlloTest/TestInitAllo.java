75389c7add8b6d9b1cf62e1285d5966293d86a66
Zoom <zoomfo@gmail.com>
2021-02-04 16:05:06 +0300

    new me test Allo site

---
+++ src/test/java/AlloTest/TestInitAllo.java
@@ -0,0 +1,50 @@
+package AlloTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class TestInitAllo{
+
+    public WebDriver driver;
+
+    @BeforeTest
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        openUrl("https://allo.ua/ru/kompjutery/");
+    }
+
+    public void openUrl(String site){
+        driver.get(site);
+    }
+
+    @AfterTest
+    public void tearDown(){
+        driver.quit();
+    }
+
+    public void pushBtnReturn(){
+        Actions action = new Actions(driver);
+        action.sendKeys(Keys.RETURN);
+        action.perform();
+    }
+
+    public void sleep(int seconds){
+        try{
+            Thread.sleep(seconds * 1000);
+        } catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void moveMouseToElement(WebElement element){
+        Actions actions = new Actions(driver);
+        actions.moveToElement (element).perform ();
+    }
+}
