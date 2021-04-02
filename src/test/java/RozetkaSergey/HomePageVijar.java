baca8e5c57ce4565b2248b253aebcc98b4781fc4
Sam <grinred69@gmail.com>
2021-04-02 12:57:58 +0300

    Vtest

--- src/test/java/RozetkaSergey/HomePageVijar.java
+++ src/test/java/RozetkaSergey/HomePageVijar.java
@@ -7,14 +7,51 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class HomePageVijar extends BasePage {
+    private String login = "grinred69@gmail.com";
+    private String pass = "23112010vi";
 
     public HomePageVijar(WebDriver driver) {
         super(driver);
     }
+
     public void enterToGoogle(String site) {
         driver.get(site);
     }
+
     public WebElement googleField() {
         return getElement("//input[@class='gLFyf gsfi']");
     }
+
+    public WebElement goToVijar() {
+        return getElement("//*[@class='LC20lb DKV0Md' and text()='Вияр - интернет-магазин мебельной фурнитуры. Все ...']");
+    }
+
+    public WebElement closeAdverb() {
+        return getElement("//img[@src='/img/close_white-24px.svg']");
+    }
+
+    public WebElement goToEnter() {
+        return getElement("//div[@class='login-in']//a[@id='modal_ancor']");
+    }
+
+    public WebElement getLogin() {
+        return getElement("//input[@class='flat_input login']");
+    }
+
+    public WebElement getPass() {
+         return getElement("//input[@class='flat_input password']");
+    }
+
+    public WebElement clickEnter() {
+        return getElement("//input[@class='login_btn']");
+    }
+    public String getterPass() {
+        return pass;
+    }
+
+    public String getterLogin() {
+        return login;
+    }
+
+
 }
--- src/test/java/RozetkaSergey/Vijar.java
+++ src/test/java/RozetkaSergey/Vijar.java
@@ -8,57 +8,22 @@ import org.testng.annotations.Test;
 
 public class Vijar extends TestInit {
 
-    String login = "grinred69@gmail.com";
-    String pass = "23112010vi";
-
     @Test
     public void loginTest() {
         HomePageVijar homePageVijar = new HomePageVijar(driver);
         homePageVijar.enterToGoogle("https://www.google.com");
         homePageVijar.googleField().sendKeys("Вияр");
         enter();
+        homePageVijar.goToVijar().click();
         sleep(1);
-        goToVijar().click();
+        homePageVijar.closeAdverb().click();
+        homePageVijar.goToEnter().click();
+        homePageVijar.getLogin().sendKeys(homePageVijar.getterLogin());
+        homePageVijar.getPass().sendKeys(homePageVijar.getterPass());
+        homePageVijar.clickEnter().click();
         sleep(1);
-        closeAdverb().click();
-        sleep(1);
-        goToEnter().click();
-        sleep(1);
-        getLogin().sendKeys(login);
-        getPass().sendKeys(pass);
-        sleep(1);
-        clickEnter().click();
-        sleep(1);
-        closeAdverb().click();
-        sleep(1);
-    }
-
-
-
-
-
-    public WebElement goToVijar() {
-        return driver.findElement(By.xpath("//*[@class='LC20lb DKV0Md' and text()='Вияр - интернет-магазин мебельной фурнитуры. Все ...']"));
-    }
-
-    public WebElement goToEnter() {
-        return driver.findElement(By.xpath("//div[@class='login-in']//a[@id='modal_ancor']"));
-    }
-
-    public WebElement closeAdverb() {
-        return driver.findElement(By.xpath("//img[@src='/img/close_white-24px.svg']"));
-    }
-
-    public WebElement getLogin() {
-        return driver.findElement(By.xpath("//input[@class='flat_input login']"));
-    }
-
-    public WebElement getPass() {
-        return driver.findElement(By.xpath("//input[@class='flat_input password']"));
-    }
-
-    public WebElement clickEnter() {
-        return driver.findElement(By.xpath("//input[@class='login_btn']"));
+        homePageVijar.closeAdverb().click();
+        System.out.println("Sehr gut!");
     }
 }
 
