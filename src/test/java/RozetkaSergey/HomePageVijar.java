9a580d8facf684e737b0b3ec1ecdf69dcadd9ea5
Sam <grinred69@gmail.com>
2021-04-05 17:55:11 +0300

    Test for Vijar 2

--- src/test/java/RozetkaSergey/HomePageVijar.java
+++ src/test/java/RozetkaSergey/HomePageVijar.java
@@ -29,19 +29,15 @@ public class HomePageVijar extends BasePage {
         return getElement(
                 "//*[@class='LC20lb DKV0Md' and text()='Вияр - интернет-магазин мебельной фурнитуры. Все ...']");
     }
-
     public WebElement closeAdverb() {
         return getElement("//img[@src='/img/close_white-24px.svg']");
     }
-
     public WebElement goToEnter() {
         return getElement("//div[@class='login-in']//a[@id='modal_ancor']");
     }
-
     public WebElement getLogin() {
         return getElement("//input[@class='flat_input login']");
     }
-
     public WebElement getPass() {
          return getElement("//input[@class='flat_input password']");
     }
@@ -56,7 +52,12 @@ public class HomePageVijar extends BasePage {
     }
     public WebElement switchToUa() { return getElement("//a[@href='/ua/']");}
     public WebElement newsHeaderElement() { return getElement("//nav[contains(@class, 'top-menu')]//a[@href='/ua/news/']");}
-
+    public WebElement clickAtToolbar () {return getElement("//li[@class='item0']");}
+    public WebElement clickToDSP (){return getElement("//a[@href='/catalog/dsp/' and text()='ДСП']");}
+    public WebElement itemForCompareOne (){return getElement("//div[@class='col-xs-12 col-sm-6 col-md-6 col-lg-4 product_prewiew-wrapper paginator-item'][1]//div[@class='compare_button']");}
+    public WebElement itemForCompareTwo (){return getElement("//div[@class='col-xs-12 col-sm-6 col-md-6 col-lg-4 product_prewiew-wrapper paginator-item'][2]//div[@class='compare_button']");}
+    public WebElement itemCounter () {return  getElement("//div[@class='back-comp']//span");}
+    public WebElement goToCompare (){return getElement("//div[@class='back-comp']//span");}
 
 
 }
