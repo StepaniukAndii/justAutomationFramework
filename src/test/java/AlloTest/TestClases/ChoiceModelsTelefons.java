cfe436eba678612bb12b0421d1c2534079f770e8
eltess <78352571+eltess@users.noreply.github.com>
2021-04-06 22:51:02 +0200

    Update ChoiceModelsTelefons.java

--- src/test/java/AlloTest/TestClases/ChoiceModelsTelefons.java
+++ src/test/java/AlloTest/TestClases/ChoiceModelsTelefons.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
-
 import java.util.List;
 
 public class ChoiceModelsTelefons extends TestInit {
@@ -14,20 +13,20 @@ public class ChoiceModelsTelefons extends TestInit {
     @Test
     public void choiceModelsTelefons() {
         LocatorsForPriceCheckTelefons locators = new LocatorsForPriceCheckTelefons (driver);
-        openUrl ("https://allo.ua/ru/");
-        locators.smartAndTelefons ().click ();
-        locators.telefons ().click ();
-        locators.samsung ().click ();
-        locators.showFilterTelefons ().click ();
+        openUrl("https://allo.ua/ru/");
+        locators.smartAndTelefons().click();
+        locators.telefons().click();
+        locators.samsung().click();
+        locators.showFilterTelefons().click();
         WebDriverWait wait = new WebDriverWait (driver, 10);
-        wait.until(ExpectedConditions.urlContains ("samsung"));
-        List<String> list = getAllElementsWithAttribute (locators.listSamsung (), "title");
+        wait.until(ExpectedConditions.urlContains("samsung"));
+        List<String> list = getAllElementsWithAttribute (locators.listSamsung(), "title");
         String  token = "Samsung";
         for(String l:list){
             if(l.contains(token)){
-                System.out.println ("Совпадение с самсунгом");
+                System.out.println("Совпадение с самсунгом");
             } else {
-                System.out.println ("Нету совпадений с самсунгом");
+                System.out.println("Нету совпадений с самсунгом");
             }
         }
     }
