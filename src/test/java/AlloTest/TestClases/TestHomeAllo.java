6f761387166aefc2b8ab5ff3b34e57bcbcece383
Zoom <zoomfo@gmail.com>
2021-04-09 16:50:06 +0200

    CheckCategories

--- src/test/java/AlloTest/TestClases/TestHomeAllo.java
+++ src/test/java/AlloTest/TestClases/TestHomeAllo.java
@@ -1,30 +1,12 @@
 package AlloTest.TestClases;
 
 import AlloTest.Pages.PageHomeAllo;
-import AlloTest.HelpClases.PersonSingIn;
 import ClasesToAllUs.TestInit;
 import org.testng.annotations.Test;
 
 public class TestHomeAllo extends TestInit {
 
     PageHomeAllo pageHomeAllo;
-
-    @Test
-    public void checkCategories(){
-        pageHomeAllo = new PageHomeAllo(driver);
-        openUrl("https://allo.ua/ru/");
-        pageHomeAllo.logo().click();
-        pageHomeAllo.categoriesClick().click();
-        pageHomeAllo.telefonItems().click();
-        pageHomeAllo.categoriesClick().click();
-        pageHomeAllo.toolsItem().click();
-        pageHomeAllo.categoriesClick().click();
-        pageHomeAllo.appLiancesItems().click();
-        pageHomeAllo.categoriesClick().click();
-        pageHomeAllo.carsItems().click();
-        pageHomeAllo.categoriesClick().click();
-        pageHomeAllo.sportItems().click();
-    }
 
     @Test
     public void testAddItemsInBasket(){
