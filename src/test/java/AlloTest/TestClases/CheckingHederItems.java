d4cd55ab7937d0c432cab85e74e567b003bd8d3c
eltess <78352571+eltess@users.noreply.github.com>
2021-04-11 11:02:59 +0200

    Update CheckingHederItems.java

--- src/test/java/AlloTest/TestClases/CheckingHederItems.java
+++ src/test/java/AlloTest/TestClases/CheckingHederItems.java
@@ -4,7 +4,6 @@ import AlloTest.Pages.CheckingProductCatalogLocators;
 import ClasesToAllUs.TestInit;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
-
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
