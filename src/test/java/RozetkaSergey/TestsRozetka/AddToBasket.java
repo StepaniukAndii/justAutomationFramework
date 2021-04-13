4c3c7bd9225bbf969d991e00a779d195a1973fec
Sam <grinred69@gmail.com>
2021-04-13 23:10:22 +0300

    RozetkaTest#4

--- src/test/java/RozetkaSergey/TestsRozetka/AddToBasket.java
+++ src/test/java/RozetkaSergey/TestsRozetka/AddToBasket.java
@@ -2,11 +2,12 @@ package RozetkaSergey.TestsRozetka;
 
 import ClasesToAllUs.TestInit;
 import RozetkaSergey.RozetkaPages.RozetkaPages1_4;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TopMenu extends TestInit {
+public class AddToBasket extends TestInit {
     @Test
-    public void topMenu(){
+    public void addtoBasket(){
         RozetkaPages1_4 rozetkaPages1_4 = new RozetkaPages1_4(driver);
         rozetkaPages1_4.enterToRozetka("https://rozetka.com.ua/");
         rozetkaPages1_4.closeAdverb().click();
@@ -15,6 +16,7 @@ public class TopMenu extends TestInit {
         sleep(1);
         rozetkaPages1_4.chooseComp().click();
         rozetkaPages1_4.addToCard().get(0).click();
-        sleep(1);
+        String digitInBasket = rozetkaPages1_4.cardDigit().getText();
+        Assert.assertEquals("1",digitInBasket);
     }
 }
