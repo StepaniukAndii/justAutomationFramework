59887f54b611ef1442540335b1180a139b40b3b3
Sam <grinred69@gmail.com>
2021-04-27 13:40:03 +0300

    Test checks Searchfield

--- src/test/java/RozetkaSergey/KickstarterPages/KickstartPage.java
+++ src/test/java/RozetkaSergey/KickstarterPages/KickstartPage.java
@@ -10,6 +10,7 @@ public class KickstartPage extends BasePage {
     public KickstartPage(WebDriver driver) {super(driver);}
     public void enterToKickStarter (String site ) {this.driver.get(site);}
     public List<WebElement> listElementsOfTopMenu (){return getElements("//ul[@class='flex justify-center-md text-nowrap']//li");}
-
+    public WebElement searchButton (){return getElement("//span[@class='display-none block-sm']");}
+    public WebElement searchField (){return getElement("//input[@class='border-box keyboard-focusable no-outline h13 h11-md medium z-dropdown-3 w100p relative radius0 pl4 pl7-md type-18 inline-block border-none shadow-none js-search-term-gs']");}
 
 }
