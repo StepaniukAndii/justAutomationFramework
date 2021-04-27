b23ae9538d845d78d4aa2b7f2bef67e322547a63
Stepaniuk <andrii.stepaniuk18@gmail.com>
2021-04-27 20:33:53 +0300

    Added test

--- src/test/java/TestRozetkaAndDOU/DOU/PagesDOU/CalendarPage.java
+++ src/test/java/TestRozetkaAndDOU/DOU/PagesDOU/CalendarPage.java
@@ -4,6 +4,8 @@ import ClasesToAllUs.BasePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 public class CalendarPage extends BasePage {
     public CalendarPage(WebDriver driver) {
         super(driver);
@@ -15,5 +17,25 @@ public class CalendarPage extends BasePage {
 
     public WebElement getArchive() {
         return getElement("//div[@class='wrap ']");
+    }
+
+    public WebElement getHeatText() {
+        return getElement("//span[@class='m-hide']");
+    }
+
+    public WebElement getSelectCity() {
+        return getElement("//select[@name='city']");
+    }
+
+    public List<WebElement> getSelectCityChildren() {
+        return getElements("//select[@name='city']/option");
+    }
+
+    public WebElement getSelectAllTags() {
+        return getElement("//select[@name='tag']");
+    }
+
+    public List<WebElement> getSelectAllTagsChildren() {
+        return getElements("//select[@name='tag']/option");
     }
 }
