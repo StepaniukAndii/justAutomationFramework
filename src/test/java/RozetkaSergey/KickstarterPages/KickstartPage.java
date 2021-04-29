16809985def65f28386dd1416a0cb5f377f4650e
Sam <grinred69@gmail.com>
2021-04-29 13:47:21 +0300

    Test changes Money Value

--- src/test/java/RozetkaSergey/KickstarterPages/KickstartPage.java
+++ src/test/java/RozetkaSergey/KickstarterPages/KickstartPage.java
@@ -14,6 +14,10 @@ public class KickstartPage extends BasePage {
     public WebElement searchField (){return getElement("//input[@class='border-box keyboard-focusable no-outline h13 h11-md medium z-dropdown-3 w100p relative radius0 pl4 pl7-md type-18 inline-block border-none shadow-none js-search-term-gs']");}
     public WebElement switchLanguageButton(){return getElement("//select[@aria-label='Change the language you browse Kickstarter with']");}
     public WebElement copyWriteAndYear (){return getElement("//div[@class='flex items-center mb3 mb0-lg']//span");}
-    //public WebElement listOfSocialNetworks1(){return getElement("//div[@class='border-bottom border2px py8']//a");}
     public List<WebElement> listOfSocialNetworks (){return getElements("//div[@class='border-bottom border2px py8']//a");}
-}
+    public WebElement someElementFromPage (){return getElement("//img[@class='w100p block']");}
+    public List<WebElement> priceList (){return getElements("//div[@class='mb4-lg']//span");}
+    public WebElement switchMoneyButton(){return getElement("//select[@name='currency']");}
+    public WebElement continueButton(){return getElement("//div[@class='shadow-low bg-white p4 max-h70vh auto-scroll-y clip']//button");}
+    public List<WebElement> theSecondListOfPrice(){return getElements("//div[@class='mb4-lg']//span[@class='ksr-green-500']");}
+    }
