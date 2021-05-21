bf7d402d5518177a50672bc1f75bd8daab6d665d
Shevchun Denis <fellowastronomer14@gmail.com>
2021-05-21 13:53:42 +0300

    Added footer test of buttons presence.

--- src/test/java/denisFoxtrotTest/pages/FooterFoxtrot.java
+++ src/test/java/denisFoxtrotTest/pages/FooterFoxtrot.java
@@ -10,4 +10,8 @@ public class FooterFoxtrot extends BasePage {
     public FooterFoxtrot(WebDriver driver) {
         super(driver);
     }
+
+    public List<WebElement> footerButtons() {
+        return findElementsByXpath("//div [@class='footer__text']");
+    }
 }
