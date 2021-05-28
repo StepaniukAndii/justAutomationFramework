ef39300a3873cf9c0c2b34739a9464eea05ab5f8
Shevchun Denis <fellowastronomer14@gmail.com>
2021-05-28 14:10:20 +0300

    Restored cartTests

---
+++ src/test/java/denisFoxtrotTest/cartTest/RemoveFromCart.java
@@ -0,0 +1,35 @@
+package denisFoxtrotTest.cartTest;
+
+import ClasesToAllUs.TestInit;
+import denisFoxtrotTest.foxPages.HomePageFoxtrot;
+import org.testng.annotations.Test;
+
+public class RemoveFromCart extends TestInit {
+
+    @Test
+    public void removeFromCart() {
+        HomePageFoxtrot homePageFoxtrot = new HomePageFoxtrot(driver);
+        openUrl("https://www.foxtrot.com.ua/");
+        homePageFoxtrot.getSearch().sendKeys("Холодильник\n");
+        homePageFoxtrot.addToCartList().get(3).click();
+        while (!homePageFoxtrot.cartTooltip().isEnabled()) {
+            homePageFoxtrot.headerCart().click();
+        }
+        homePageFoxtrot.openCart().click();
+        int size = homePageFoxtrot.removeInsideCart().size();
+        int remove = 0;
+        for (int i = 0; i < size; i++) {
+            homePageFoxtrot.removeInsideCart().get(remove).click();
+        }
+        driver.navigate().back();
+        homePageFoxtrot.addToCartList().get(2).click();
+        while (!homePageFoxtrot.cartTooltip().isEnabled()) {
+            homePageFoxtrot.headerCart().click();
+        }
+        int merchAmount = homePageFoxtrot.removeFromCartTooltip().size();
+        for (int i = 0; i < merchAmount; i++) {
+            homePageFoxtrot.removeFromCartTooltip().get(remove).click();
+            sleep(1);
+        }
+    }
+}
--- src/test/java/denisFoxtrotTest/foxPages/HomePageFoxtrot.java
+++ src/test/java/denisFoxtrotTest/foxPages/HomePageFoxtrot.java
@@ -23,7 +23,32 @@ public class HomePageFoxtrot extends BasePage {
     public WebElement addToCart() {
         return findElementByXpath("//div[@class='button buy-button product-box__main-buy__button product-buy-button']");
     }
+
     public WebElement nextPage() {
         return findElementByXpath("//i [@class='icon icon-chevron-right']");
+    }
+
+    public List<WebElement> addToCartList() {
+        return findElementsByXpath("//div [@class='button buy-button card__buttons-row product-buy-button']");
+    }
+
+    public WebElement headerCart() {
+        return findElementByXpath("//i [@class='header-basket__icon icon-shop js-toggle-tooltip']");
+    }
+
+    public WebElement cartTooltip() {
+        return findElementByXpath("//i [@id='cartTooltipClose']");
+    }
+
+    public WebElement openCart() {
+        return findElementByXpath("//a [@href='/ru/cart.html']");
+    }
+
+    public List<WebElement> removeInsideCart() {
+        return findElementsByXpath("//a [@class='button-link shop-cart__item-nav__link remove-from-cart']");
+    }
+
+    public List<WebElement> removeFromCartTooltip() {
+        return findElementsByXpath("//a [@class='button-link js-delete-product-from-basket-list']");
     }
 }
