a05ffc3202354ad015b3e8555e534b9606d4ed4b
Angelina Ilchuk <ilchukangelina@gmail.com>
2021-06-16 16:18:44 +0300

    reload 5 tests of Asos

--- src/test/java/TestAsosByLynn/AsosPages.java
+++ src/test/java/TestAsosByLynn/AsosPages.java
@@ -9,38 +9,38 @@ public class AsosPages extends BasePage {
 public AsosPages(WebDriver driver) {super(driver);}
 
     //TestNewUserRegistration
-    public WebElement regIcon() {return getElement("//div[@id='myAccountDropdown']");}
-    public List<WebElement> logInOrJoin() {return getElements("//a[@class='_1336dMe _1uUU2Co _1336dMe _1uUU2Co']");}
-    public WebElement emailInPut() {return getElement("//input[@id='Email']");}
-    public WebElement firsNameInPut() {return getElement("//input[@id='FirstName']");}
-    public WebElement lastNameInPut() {return getElement("//input[@id='LastName']");}
-    public WebElement passwordInPut() {return getElement("//input[@id='Password']");}
-    public WebElement birthDay() {return getElement("//select[@id='BirthDay']");}
-    public WebElement birthMonth() {return getElement("//select[@id='BirthMonth']");}
-    public WebElement birthYear() {return getElement("//select[@id='BirthYear']");}
-    public WebElement regBtn() {return getElement("//input[@id='register']");}
+    public WebElement regIcon() {return findElementByXpath("//div[@id='myAccountDropdown']");}
+    public List<WebElement> logInOrJoin() {return findElementsByXpath("//a[@class='_1336dMe _1uUU2Co _1336dMe _1uUU2Co']");}
+    public WebElement emailInPut() {return findElementByXpath("//input[@id='Email']");}
+    public WebElement firsNameInPut() {return findElementByXpath("//input[@id='FirstName']");}
+    public WebElement lastNameInPut() {return findElementByXpath("//input[@id='LastName']");}
+    public WebElement passwordInPut() {return findElementByXpath("//input[@id='Password']");}
+    public WebElement birthDay() {return findElementByXpath("//select[@id='BirthDay']");}
+    public WebElement birthMonth() {return findElementByXpath("//select[@id='BirthMonth']");}
+    public WebElement birthYear() {return findElementByXpath("//select[@id='BirthYear']");}
+    public WebElement regBtn() {return findElementByXpath("//input[@id='register']");}
     //TestLogInExistingUser
-    public WebElement emailInPutLogInForm() {return getElement("//input[@id='EmailAddress']");}
-    public WebElement signInBtn() {return getElement("//input[@id='signin']");}
-    public List<WebElement> errorBlock() {return getElements("//div[@class='error-block']");}
-    public WebElement logInGreeting() {return getElement("//span[@class='tiqiyps']");}
+    public WebElement emailInPutLogInForm() {return findElementByXpath("//input[@id='EmailAddress']");}
+    public WebElement signInBtn() {return findElementByXpath("//input[@id='signin']");}
+    public List<WebElement> errorBlock() {return findElementsByXpath("//div[@class='error-block']");}
+    public WebElement logInGreeting() {return findElementByXpath("//span[@class='tiqiyps']");}
     //TestCart
-    public WebElement searchField() {return getElement("//input[@id='chrome-search']");}
-    public List<WebElement> itemsSocks() {return getElements("//a[@class='_3TqU78D']");}
-    public WebElement sizeSelect() {return getElement("//select[@data-id='sizeSelect']");}
-    public WebElement addToBagBtn() {return getElement("//button[@data-test-id='add-button']");}
-    public WebElement cartBtn() {return getElement("//span[@class='_1z5n7CN']");}
-    public WebElement nameOfTheChosenProduct() {return getElement("//div[@class='layout-aside']//h1");}
-    public WebElement nameOfTheChosenProductInCart() {return getElement("//p[@class='bag-item-name']//a");}
-    public WebElement bagLink() {return getElement("//a[@data-test-id='bag-link']");}
+    public WebElement searchField() {return findElementByXpath("//input[@id='chrome-search']");}
+    public List<WebElement> itemsSocks() {return findElementsByXpath("//a[@class='_3TqU78D']");}
+    public WebElement sizeSelect() {return findElementByXpath("//select[@data-id='sizeSelect']");}
+    public WebElement addToBagBtn() {return findElementByXpath("//button[@data-test-id='add-button']");}
+    public WebElement cartBtn() {return findElementByXpath("//span[@class='_1z5n7CN']");}
+    public WebElement nameOfTheChosenProduct() {return findElementByXpath("//div[@class='layout-aside']//h1");}
+    public WebElement nameOfTheChosenProductInCart() {return findElementByXpath("//p[@class='bag-item-name']//a");}
+    public WebElement bagLink() {return findElementByXpath("//a[@data-test-id='bag-link']");}
     //TestFilters
-    public WebElement brandFilter() {return getElement("//li[@data-auto-id='brand']");}
-    public List<WebElement> brands() {return getElements("//div[@class='kx2nDmW']");}
-    public WebElement selectedBrand() {return getElement("//p[@data-auto-id='selectedFacetValueList']");}
+    public WebElement brandFilter() {return findElementByXpath("//li[@data-auto-id='brand']");}
+    public List<WebElement> brands() {return findElementsByXpath("//div[@class='kx2nDmW']");}
+    public WebElement selectedBrand() {return findElementByXpath("//p[@data-auto-id='selectedFacetValueList']");}
     //TestFieldValidation
-    public List<WebElement> emailError() {return getElements("//span[@id='Email-error']");}
-    public List<WebElement> firstNameError() {return getElements("//span[@id='FirstName-error']");}
-    public List<WebElement> lastNameError() {return getElements("//span[@id='LastName-error']");}
-    public List<WebElement> passwordError() {return getElements("//span[@id='Password-error']");}
-    public List<WebElement> birthError() {return getElements("//span[@id='BirthYear-error']");}
+    public List<WebElement> emailError() {return findElementsByXpath("//span[@id='Email-error']");}
+    public List<WebElement> firstNameError() {return findElementsByXpath("//span[@id='FirstName-error']");}
+    public List<WebElement> lastNameError() {return findElementsByXpath("//span[@id='LastName-error']");}
+    public List<WebElement> passwordError() {return findElementsByXpath("//span[@id='Password-error']");}
+    public List<WebElement> birthError() {return findElementsByXpath("//span[@id='BirthYear-error']");}
 }
--- src/test/java/TestEpicentrByLynn/HomePage.java
+++ src/test/java/TestEpicentrByLynn/HomePage.java
@@ -8,23 +8,16 @@ import java.util.List;
 public class HomePage extends BasePage {
 public HomePage(WebDriver driver) {super(driver);}
     public WebElement popupWindow() {
-        return  getElement("//span[@class='_2qGxAC']"); }
+        return  findElement("//span[@class='_2qGxAC']"); }
     public WebElement searchField() {
-        return getElement("//input[@class='field-container__field']");}
+        return findElement("//input[@class='field-container__field']");}
     public List<WebElement> buyBtns() {
-        return getElements("//button[@class='add-product card__button " +
+        return findElementsByXpath("//button[@class='add-product card__button " +
                 "btn btn--yellow  js-btn--buy ']");}
     public WebElement continueShopping() {
-        return getElement("//button[@class='btn btn--3 btn--white ']");}
+        return findElement("//button[@class='btn btn--3 btn--white ']");}
     public List<WebElement> priceList() {
-        return getElements("//p[@class='basket-product__price-main']");}
+        return findElementsByXpath("//p[@class='basket-product__price-main']");}
     public WebElement cartBtn() {
-        return getElement("//span[@class='header__cart-icon']");}
-    //sale test
-    public List<WebElement> saleStickers() {return getElements("//section[@class='container productList" +
-            "Conteiner product-list-wrapper']//span[@class='sticker action']");}
-    public WebElement showMore() {
-        return getElement("//a[@class='card card--preload link link--big link--inverted link--blue']");}
-    public List<WebElement> pageNumber() {
-        return getElements("//a[@class='pagination__item']");}
+        return findElement("//span[@class='header__cart-icon']");}
 }
