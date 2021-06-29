f441521de4f87845f70af35f1f77ca8d1102d598
Shevchun Denis <fellowastronomer14@gmail.com>
2021-06-29 13:39:07 +0300

    Added footer test

---
+++ src/test/java/testRozetkaDenis/pages/FooterPage.java
@@ -0,0 +1,47 @@
+package testRozetkaDenis.pages;
+
+import ClasesToAllUs.BasePage;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class FooterPage extends BasePage {
+    public FooterPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public HeaderPage headerPageFunctions() {
+        return new HeaderPage(driver);
+    }
+
+    public ProductPage productPageFunctions() {
+        return new ProductPage(driver);
+    }
+
+    public WebElement callCenterGraphics() {
+        return findElementByXpath("//a[@class='button button--small button--with-icon button--link footer-top__schedule']");
+    }
+
+    public List<WebElement> footerInfo() {
+        return findElementsByXpath("//a [@class='ng-tns-c20-1']");
+    }
+
+    public List<WebElement> footerApps() {
+        return findElementsByXpath("//a [@class='footer-stores__button']");
+    }
+
+    public List<WebElement> footerSocials() {
+        return findElementsByXpath("//li [@class='socials__list-item ng-star-inserted']");
+    }
+
+    public List<WebElement> footerPayments() {
+        return findElementsByXpath("//button [@class='payments-buttons__button']");
+    }
+
+    public CharSequence openNewTab() {
+        String openTab;
+        return openTab = Keys.chord(Keys.CONTROL, Keys.RETURN);
+    }
+}
