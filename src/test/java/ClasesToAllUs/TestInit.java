1fc27aec4281228bc04c0dc0602bf64627cef008
Shevchun Denis <fellowastronomer14@gmail.com>
2021-07-01 12:02:27 +0300

    Big fix of Rozetka test

--- src/test/java/ClasesToAllUs/TestInit.java
+++ src/test/java/ClasesToAllUs/TestInit.java
@@ -4,13 +4,11 @@ import RozetkaSergey.RozetkaPages.RozetkaPages5_34;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
-import testRozetkaDenis.pages.FooterPage;
 import testRozetkaDenis.pages.ProductPage;
 
 import java.util.LinkedList;
