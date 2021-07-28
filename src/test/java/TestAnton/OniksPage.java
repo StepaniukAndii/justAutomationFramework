1823bb0a97c98cb25dc015b41e92763e4f1bf87b
Anton Chuvirov <anton.4uvirov@gmail.com>
2021-07-28 16:55:22 +0300

    TestLogoBtn

--- src/test/java/TestAnton/OniksPage.java
+++ src/test/java/TestAnton/OniksPage.java
@@ -10,4 +10,7 @@ public class OniksPage extends BasePage {
     public WebElement searchField() {return findElementByXpath("//ul//div[@class='search-wrap']");}
     public WebElement searchFieldLong() {return findElementByXpath("//ul//input[@id='title-search-input']");}
     public WebElement backOnHomePageButton() {return findElementByXpath("//span[@itemprop='name']");}
+    public WebElement aboutUsBtnHeader() {return findElementByXpath("//ul[@class='nav navbar-nav']" +
+            "//a[@href='/about/']");}
+    public WebElement homePageLogoBtn() {return findElementByXpath("//img[@width='250']");}
 }
--- src/test/java/TestAnton/TestLogoBtn.java
+++ src/test/java/TestAnton/TestLogoBtn.java
@@ -6,6 +6,9 @@ import org.testng.annotations.Test;
 public class TestLogoBtn extends TestInit {
     @Test
     public void testLogoBtn() {
-
+        OniksPage homePage = new OniksPage(driver);
+        driver.get("https://oniks.ua");
+        homePage.aboutUsBtnHeader().click();
+        homePage.homePageLogoBtn().click();
     }
 }
