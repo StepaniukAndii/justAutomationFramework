e17d40aa64281afb2377865ad2015252ad2c4122
DenisShevchun <83283956+DenisShevchun@users.noreply.github.com>
2021-07-28 16:03:05 +0300

    Update HomePage.java

--- src/test/java/testEatStreetDenis/pages/HomePage.java
+++ src/test/java/testEatStreetDenis/pages/HomePage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class HomePage extends BasePage {
+    
     public HomePage(WebDriver driver) {
         super(driver);
     }
@@ -24,7 +25,4 @@ public class HomePage extends BasePage {
     public WebElement cart() {
         return findElementByXpath("//li [@class='is-clickable']");
     }
-
-
-
 }
