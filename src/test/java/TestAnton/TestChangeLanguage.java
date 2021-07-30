6d86e15a31124945e758aedbd29ec72267416237
Anton Chuvirov <anton.4uvirov@gmail.com>
2021-07-30 10:18:58 +0300

    TestChangeLanguage

---
+++ src/test/java/TestAnton/TestChangeLanguage.java
@@ -0,0 +1,20 @@
+package TestAnton;
+
+import ClasesToAllUs.TestInit;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestChangeLanguage extends TestInit {
+    @Test
+    public void testChangeLanguage(){
+        OniksPage homePage = new OniksPage(driver);
+        driver.get("https://oniks.ua");
+        homePage.languageWindow().click();
+        homePage.textUALanguage().click();
+        Assert.assertTrue(homePage.wordCartOnUA().getText().contains("Кошик"));
+        homePage.languageWindow().click();
+        homePage.textRULanguage().click();
+        Assert.assertTrue(homePage.wordCartOnRU().getText().contains("Корзина"));
+    }
+
+}
