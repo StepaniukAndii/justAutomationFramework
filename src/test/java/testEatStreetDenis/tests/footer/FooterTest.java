923fcef99f3b265b2abbbf49baacd867d5176072
Shevchun Denis <fellowastronomer14@gmail.com>
2021-08-03 15:08:32 +0300

    [eatstreet+] refactoring footer tests

--- src/test/java/testEatStreetDenis/tests/footer/FooterTest.java
+++ src/test/java/testEatStreetDenis/tests/footer/FooterTest.java
@@ -13,26 +13,28 @@ public class FooterTest extends TestInit {
     public void footerTest() {
         footerPage = new FooterPage(driver);
         openUrl("https://qa2.eatstreet.com/");
+
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0,250)", "");
-        for (int i = 0; i < footerPage.footerSocials().size(); i++) {
-            footerPage.footerSocials().get(i).sendKeys(footerPage.openNewTab());
+
+        for (int i = 0; i < footerPage.getFooterSocials().size(); i++) {
+            footerPage.getFooterSocials().get(i).sendKeys(footerPage.openNewTab());
         }
-        footerElements(0, 6);
-        footerSpecialElements(7);
-        footerElements(8, 9);
-        footerPage.footerElements().get(10).click();
-        footerElements(11, 16);
+        openFooterElementsInRange(0, 6);
+        openFooterSpecialElements(7);
+        openFooterElementsInRange(8, 9);
+        footerPage.getFooterElements().get(10).click();
+        openFooterElementsInRange(11, 16);
     }
 
-    private void footerElements(Integer startElement, Integer lastElement) {
+    private void openFooterElementsInRange(Integer startElement, Integer lastElement) {
         for (int i = startElement; i <= lastElement; i++) {
-            footerPage.footerElements().get(i).sendKeys(footerPage.openNewTab());
+            footerPage.getFooterElements().get(i).sendKeys(footerPage.openNewTab());
         }
     }
 
-    private void footerSpecialElements(Integer element) {
-        footerPage.footerElements().get(element).click();
+    private void openFooterSpecialElements(Integer element) {
+        footerPage.getFooterElements().get(element).click();
         driver.navigate().back();
     }
 }
--- src/test/java/testEatStreetDenis/tests/footer/support/GetHelpTest.java
+++ src/test/java/testEatStreetDenis/tests/footer/support/GetHelpTest.java
@@ -13,19 +13,18 @@ public class GetHelpTest extends TestInit {
         supportPage = new SupportPage(driver);
         openUrl("https://qa2.eatstreet.com/support-request-modal");
 
-        supportPage.getHelpFirstName().sendKeys("First");
-        supportPage.getHelpLastName().sendKeys("Last");
-        //checking reaction on wrong email or phone number
-        supportPage.getHelpEmail().sendKeys("sg@gmail.com");
-        supportPage.getHelpPhone().sendKeys("16505130514");
-        supportPage.getHelpMessage().sendKeys("Test Message");
-        supportPage.submit().click();
-        supportPage.getHelpSubject().click();
+        supportPage.getHelpFirstNameInput().sendKeys("First");
+        supportPage.getHelpLastNameInput().sendKeys("Last");
+        supportPage.getHelpEmailInput().sendKeys("sg@gmail.com");
+        supportPage.getHelpPhoneInput().sendKeys("16505130514");
+        supportPage.getHelpMessageInput().sendKeys("Test Message");
+        supportPage.submitBtn().click();
+        supportPage.getHelpSubjectSelect().click();
         supportPage.getHelpSubjectList().get(2).click();
         supportPage.getHelpChargeAmount().sendKeys("300.00");
         supportPage.getHelpChargeDate().sendKeys("12/23/2020");
         supportPage.getHelpNameOnCard().sendKeys("Name");
         supportPage.landingContentPage().acceptCookies().click();
-        supportPage.submit().click();
+        supportPage.submitBtn().click();
     }
 }
