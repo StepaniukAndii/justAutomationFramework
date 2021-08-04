193e2acfdcf5f23679f30a802a1fc7834b07cd97
Anton Chuvirov <anton.4uvirov@gmail.com>
2021-08-04 18:09:43 +0300

    Refactor all xpath locators

--- src/test/java/TestAnton/OniksPage.java
+++ src/test/java/TestAnton/OniksPage.java
@@ -9,37 +9,37 @@ import java.util.List;
 public class OniksPage extends BasePage {
     public OniksPage(WebDriver driver) {super(driver);}
 
-    public WebElement searchField() {return findElementByXpath("//ul//div[@class='search-wrap']"); }
-    public WebElement searchFieldLong() {return findElementByXpath("//ul//input[@id='title-search-input']");}
-    public WebElement backOnHomePageButton() {return findElementByXpath("//span[@itemprop='name']");}
-    public WebElement aboutUsBtnHeader() {return findElementByXpath("//ul[@class='nav navbar-nav']//a[@href='/about/']");}
-    public WebElement homePageLogoBtn() {return findElementByXpath("//img[@width='250']");}
-    public WebElement textBlog() {return findElementByXpath("//h3[contains(text(),'Блог ')]");}
-    public WebElement textReviews() {return findElementByXpath("//h3[text()='Отзывы']");}
-    public WebElement textOnlineJewelryStore() {return findElementByXpath("//h1[text()='Интернет-магазин ювелирных изделий']");}
-    public WebElement textSubscribeToOurInstagram() {return findElementByXpath("//span[text()='Подписывайтесь на наш Instagram:']");}
-    public List<WebElement> itemsRings() {return findElementsByXpath("//div[@class='col-md-3 col-xs-6 cat-item search_item_block card-mod']");}
-    public WebElement addToFavoriteBtn() {return findElementByXpath("//span[@class='favorites add-to-favourite']");}
-    public WebElement goToFavoriteBtn() {return findElementByXpath("//a[contains(text(),'Перейти в список избранных')]");}
-    public WebElement deleteFromFavoriteBtn() {return findElementByXpath("//a[@class='delete']");}
-    public WebElement textListEmpty() {return findElementByXpath("//font[@class='notetext']");}
-    public WebElement languageWindow() {return findElementByXpath("//a[@style='background-color: transparent;font-weight: bold;']");}
+    public WebElement searchField() {return findElementByXpath("//li//div[contains(@class,'search-wrap')]"); }
+    public WebElement searchFieldLong() {return findElementByXpath("//ul//input[contains(@id,'title-search-input')]");}
+    public WebElement backOnHomePageButton() {return findElementByXpath("//span[contains(text(),'ага')]");}
+    public WebElement aboutUsBtnHeader() {return findElementByXpath("//nav//a[contains(text(),'ас')]");}
+    public WebElement homePageLogoBtn() {return findElementByXpath("//a[contains(@class,'logo')]//child::img");}
+    public WebElement textBlog() {return findElementByXpath("//h3[contains(text(),'ло')]");}
+    public WebElement textReviews() {return findElementByXpath("//h3[contains(text(),'ывы')]");}
+    public WebElement textOnlineJewelryStore() {return findElementByXpath("//h1[contains(text(),'ага')]");}
+    public WebElement textSubscribeToOurInstagram() {return findElementByXpath("//span[contains(text(),'nsta')]");}
+    public List<WebElement> itemsRings() {return findElementsByXpath("//div[contains(@class,'cat-item')]");}
+    public WebElement addToFavoriteBtn() {return findElementByXpath("//span[contains(@class,'favo')]");}
+    public WebElement goToFavoriteBtn() {return findElementByXpath("//a[contains(text(),'список')]");}
+    public WebElement deleteFromFavoriteBtn() {return findElementByXpath("//a[contains(@class,'delete')]");}
+    public WebElement textListEmpty() {return findElementByXpath("//font[contains(text(),'писок')]");}
+    public WebElement languageWindow() {return findElementByXpath("//li[contains(@class,'active')]//child::a");}
     public WebElement textRULanguage() {return findElementByXpath("//a[contains(text(),'RU')]");}
     public WebElement textUALanguage() {return findElementByXpath("//a[contains(text(),'UA')]");}
     public WebElement wordCartOnUA() {return findElementByXpath("//a[contains(text(),'Кошик')]");}
     public WebElement wordCartOnRU() {return findElementByXpath("//a[contains(text(),'Корзина')]");}
-    public List<WebElement> contextMenuOnHomePage() {return findElementsByXpath("//span[@class='dropdown-toggle']");}
+    public List<WebElement> contextMenuOnHomePage() {return findElementsByXpath("//span[contains(@class,'dropdown-toggle')]");}
     public List<WebElement> collectionItems() {return findElementsByXpath("//a[contains(@class,'two-link')]//*[contains(text(),'крашен') or contains(text(),'оллекц') or contains(text(),'аспродажа')]");}
-    public List<WebElement> itemsWithAmethyst() {return findElementsByXpath("//div[@class='col-md-4 col-xs-6 cat-item card-mod']");}
-    public WebElement plusSomeItemsBtn() {return findElementByXpath("//div[@id='pagenav-button']");}
-    public WebElement allCollectionBtn() {return findElementByXpath("//a[contains(text(),'ВСЕ Коллекции')]");}
-    public WebElement filterKindOfDecorationBtn() {return findElementByXpath("//div[text()[normalize-space()='Вид украшения']]");}
+    public List<WebElement> itemsWithAmethyst() {return findElementsByXpath("//div[contains(@class,'cat-item')]");}
+    public WebElement plusSomeItemsBtn() {return findElementByXpath("//div[contains(@id,'pagenav-button')]");}
+    public WebElement allCollectionBtn() {return findElementByXpath("//a[contains(text(),'оллекции')]");}
+    public WebElement filterKindOfDecorationBtn() {return findElementByXpath("//div[contains(text(),'ид')]");}
     public WebElement filterBraceletesInCollections() {return findElementByXpath("//span[@title='Браслеты ']");}
     public WebElement showBtn() {return findElementByXpath("//div[@id='modef']//child::a");}
-    public List<WebElement> itemsBracelets() {return findElementsByXpath("//div[contains(@class,'col-md-4 col-xs-6 cat-item card-mod')]");}
-    public WebElement addToCartBtn() {return findElementByXpath("//button[@class='buy buy-button']");}
-    public WebElement closeCartAddedModal() {return findElementByXpath("//div[@id='basket_added_modal']//span[@data-dismiss='modal']");}
-    public WebElement cartBtn() {return findElementByXpath("//div[@id='blockcart-mobile']//*[contains(text(),'Корзина')]");}
-    public WebElement nameOfTheChosenProduct() {return findElementByXpath("//ul[contains(@class, 'itua_breadcrum')]//span[contains(text(), 'ольцо')]");}
+    public List<WebElement> itemsBracelets() {return findElementsByXpath("//div[contains(@class,'cat-item')]");}
+    public WebElement addToCartBtn() {return findElementByXpath("//button[contains(@class,'buy-button')]");}
+    public WebElement closeCartAddedModal() {return findElementByXpath("//div[@id='basket_added_modal']//span[contains(@class,'close')]");}
+    public WebElement cartBtn() {return findElementByXpath("//div[contains(@id,'blockcart-')]//child::li");}
+    public WebElement nameOfTheChosenProduct() {return findElementByXpath("//ul[contains(@class,'itua_breadcrum')]//span[contains(text(),'ольцо')]");}
     public WebElement nameOfTheChosenProductInCart() {return findElementByXpath("//td[@class='item_name-wrapper']//a[contains(text(),'ольцо')]");}
 }
