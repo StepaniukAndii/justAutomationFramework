77cd466eeb7298ad198c1309a8239af8a2e232e8
Anton Chuvirov <anton.4uvirov@gmail.com>
2021-08-04 16:43:51 +0300

    Refactoring xpath locators and add comment to sleep in test

--- src/test/java/TestAnton/OniksPage.java
+++ src/test/java/TestAnton/OniksPage.java
@@ -12,52 +12,34 @@ public class OniksPage extends BasePage {
     public WebElement searchField() {return findElementByXpath("//ul//div[@class='search-wrap']"); }
     public WebElement searchFieldLong() {return findElementByXpath("//ul//input[@id='title-search-input']");}
     public WebElement backOnHomePageButton() {return findElementByXpath("//span[@itemprop='name']");}
-    public WebElement aboutUsBtnHeader() {return findElementByXpath("//ul[@class='nav navbar-nav']" +
-            "//a[@href='/about/']");}
+    public WebElement aboutUsBtnHeader() {return findElementByXpath("//ul[@class='nav navbar-nav']//a[@href='/about/']");}
     public WebElement homePageLogoBtn() {return findElementByXpath("//img[@width='250']");}
     public WebElement textBlog() {return findElementByXpath("//h3[contains(text(),'Блог ')]");}
     public WebElement textReviews() {return findElementByXpath("//h3[text()='Отзывы']");}
-    public WebElement textOnlineJewelryStore() {return findElementByXpath("//h1[text()='Интернет-магазин " +
-            "ювелирных изделий']");}
-    public WebElement textSubscribeToOurInstagram() {return findElementByXpath("//span[text()='Подписывайтесь на " +
-            "наш Instagram:']");}
-    public List<WebElement> itemsRings() {return findElementsByXpath("//div[@class='col-md-3 col-xs-6 cat-item " +
-            "search_item_block card-mod']");}
-    public WebElement addToFavoriteBtn() {return findElementByXpath("//span[@class='favorites " +
-            "add-to-favourite']");}
-    public WebElement goToFavoriteBtn() {return findElementByXpath("//a[contains(text(),'Перейти в список " +
-            "избранных')]");}
+    public WebElement textOnlineJewelryStore() {return findElementByXpath("//h1[text()='Интернет-магазин ювелирных изделий']");}
+    public WebElement textSubscribeToOurInstagram() {return findElementByXpath("//span[text()='Подписывайтесь на наш Instagram:']");}
+    public List<WebElement> itemsRings() {return findElementsByXpath("//div[@class='col-md-3 col-xs-6 cat-item search_item_block card-mod']");}
+    public WebElement addToFavoriteBtn() {return findElementByXpath("//span[@class='favorites add-to-favourite']");}
+    public WebElement goToFavoriteBtn() {return findElementByXpath("//a[contains(text(),'Перейти в список избранных')]");}
     public WebElement deleteFromFavoriteBtn() {return findElementByXpath("//a[@class='delete']");}
     public WebElement textListEmpty() {return findElementByXpath("//font[@class='notetext']");}
-    public WebElement languageWindow() {return findElementByXpath("//a[@style='background-color: transparent;" +
-            "font-weight: bold;']");}
+    public WebElement languageWindow() {return findElementByXpath("//a[@style='background-color: transparent;font-weight: bold;']");}
     public WebElement textRULanguage() {return findElementByXpath("//a[contains(text(),'RU')]");}
     public WebElement textUALanguage() {return findElementByXpath("//a[contains(text(),'UA')]");}
     public WebElement wordCartOnUA() {return findElementByXpath("//a[contains(text(),'Кошик')]");}
     public WebElement wordCartOnRU() {return findElementByXpath("//a[contains(text(),'Корзина')]");}
-    public List<WebElement> contextMenuOnHomePage() {return findElementsByXpath("//span[@class=" +
-            "'dropdown-toggle']");}
-    public List<WebElement> collectionItems() {return findElementsByXpath("//span[contains(text(),'Украшения') " +
-            "or contains(text(),'Коллекция') or contains(text(),'Распродажа')]");}
-    public List<WebElement> itemsWithAmethyst() {return findElementsByXpath("//div[@class='col-md-4 col-xs-6 " +
-            "cat-item card-mod']");}
+    public List<WebElement> contextMenuOnHomePage() {return findElementsByXpath("//span[@class='dropdown-toggle']");}
+    public List<WebElement> collectionItems() {return findElementsByXpath("//a[contains(@class,'two-link')]//*[contains(text(),'крашен') or contains(text(),'оллекц') or contains(text(),'аспродажа')]");}
+    public List<WebElement> itemsWithAmethyst() {return findElementsByXpath("//div[@class='col-md-4 col-xs-6 cat-item card-mod']");}
     public WebElement plusSomeItemsBtn() {return findElementByXpath("//div[@id='pagenav-button']");}
-
     public WebElement allCollectionBtn() {return findElementByXpath("//a[contains(text(),'ВСЕ Коллекции')]");}
-    public WebElement filterKindOfDecorationBtn() {return findElementByXpath("//div[text()[normalize-space()=" +
-            "'Вид украшения']]");}
+    public WebElement filterKindOfDecorationBtn() {return findElementByXpath("//div[text()[normalize-space()='Вид украшения']]");}
     public WebElement filterBraceletesInCollections() {return findElementByXpath("//span[@title='Браслеты ']");}
     public WebElement showBtn() {return findElementByXpath("//div[@id='modef']//child::a");}
-    public List<WebElement> itemsBracelets() {return findElementsByXpath("//a[@class='js_save-element' and " +
-            "contains(text(),'браслет') or @class='js_save-element' and contains(text(),'Браслет')]");}
-  
+    public List<WebElement> itemsBracelets() {return findElementsByXpath("//div[contains(@class,'col-md-4 col-xs-6 cat-item card-mod')]");}
     public WebElement addToCartBtn() {return findElementByXpath("//button[@class='buy buy-button']");}
-    public WebElement closeCartAddedModal() {return findElementByXpath("//div[@id='basket_added_modal']" +
-            "//child::div//child::div//child::div//child::span");}
-    public WebElement cartBtn() {return findElementByXpath("//div[@id='blockcart-mobile']//child::div//child::ul" +
-            "//child::li//child::a");}
-    public WebElement nameOfTheChosenProduct() {return findElementByXpath("//h1[contains(text(),'Серебряное " +
-            "кольцо с жемчугом и фианитами 1801/1р-PWT')]");}
-    public WebElement nameOfTheChosenProductInCart() {return findElementByXpath("//a[contains(text()," +
-            "'Серебряное кольцо с жемчугом и фианитами 1801/1р-PWT')]");}
+    public WebElement closeCartAddedModal() {return findElementByXpath("//div[@id='basket_added_modal']//span[@data-dismiss='modal']");}
+    public WebElement cartBtn() {return findElementByXpath("//div[@id='blockcart-mobile']//*[contains(text(),'Корзина')]");}
+    public WebElement nameOfTheChosenProduct() {return findElementByXpath("//ul[contains(@class, 'itua_breadcrum')]//span[contains(text(), 'ольцо')]");}
+    public WebElement nameOfTheChosenProductInCart() {return findElementByXpath("//td[@class='item_name-wrapper']//a[contains(text(),'ольцо')]");}
 }
