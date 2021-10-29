fe5e7532b2be38b859dcd59aa41308ee008186c7
aleksandr <aleksandr.kharchenko35@gmail.com>
2021-10-29 16:24:57 +0300

    New CromeDriver_95 and Assert from tittle EatStreet

--- src/test/java/AleksandrKharchenko/TestGit.java
+++ src/test/java/AleksandrKharchenko/TestGit.java
@@ -1,4 +1,14 @@
 package AleksandrKharchenko;
 
-public class TestGit {
+import ClasesToAllUs.TestInit;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestGit extends TestInit {
+
+    @Test
+    public void testEatStreet(){
+        openUrl("https://eatstreet.com/");
+        Assert.assertEquals(driver.getTitle(),"Order Food Online Near You | EatStreet.com");
+    }
 }
