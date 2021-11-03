507470e4c91b983a5bbc0e7d3350ea6585975271
aleksandr <aleksandr.kharchenko35@gmail.com>
2021-11-03 17:27:58 +0200

    checking error notification on Sign In page

---
+++ src/test/java/AleksandrKharchenko/Pages/HomePage.java
@@ -0,0 +1,10 @@
+package AleksandrKharchenko.Pages;
+
+import ClasesToAllUs.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends BasePage {
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+}
---
+++ src/test/java/AleksandrKharchenko/Tests/TestErrorMessenger.java
@@ -0,0 +1,22 @@
+package AleksandrKharchenko.Tests;
+import AleksandrKharchenko.Pages.SignInPage;
+import ClasesToAllUs.TestInit;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestErrorMessenger extends TestInit {
+
+    @Test
+    public void testCheckErrorNotificationOnSignInPage() {
+        SignInPage signInPage = new SignInPage(driver);
+        signInPage.navigateSignIn();
+        signInPage.getEmailField().sendKeys("dhsehaehaegareg");
+        signInPage.getPasswordField().sendKeys("dhsehaehaegareg");
+        signInPage.closeModal();
+        signInPage.getSignInBtn().click();
+        sleep(2);
+        Assert.assertTrue(signInPage.getErrorMSG().isDisplayed());
+    }
+
+
+}
