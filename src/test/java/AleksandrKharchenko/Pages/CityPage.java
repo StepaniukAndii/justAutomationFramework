c7e84b6dbb0815daa4344143f223bf6aca5efbc8
aleksandr <aleksandr.kharchenko35@gmail.com>
2021-11-08 18:53:30 +0200

    сhoose Valentino Cafe in Los Angeles

---
+++ src/test/java/AleksandrKharchenko/Pages/CityPage.java
@@ -0,0 +1,16 @@
+package AleksandrKharchenko.Pages;
+
+import ClasesToAllUs.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CityPage extends BasePage {
+    public CityPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public WebElement getCoffeeTeaInLosAngeles() {
+        return driver.findElement(By.xpath("(//ul[@class='cuisine-types__list']//li)[6]"));
+    }
+}
