c1ec8f6bce22c05edd7a9b618b52ad31ccc530d5
Serghiy <serg17@ukr.net>
2021-11-16 17:43:19 +0200

    Confirmation password Test

--- src/test/java/SerhiiBondar/Pages/SingUpEatstreetPage.java
+++ src/test/java/SerhiiBondar/Pages/SingUpEatstreetPage.java
@@ -9,22 +9,32 @@ public class SingUpEatstreetPage extends BasePage {
 
     public SingUpEatstreetPage(WebDriver driver) {
         super(driver);
+
     }
 
     public WebElement getSingUpEmailField(){
         return driver.findElement(By.xpath("//input[@id='email']"));
+
     }
     public WebElement getPasswordForSignUpField(){
         return driver.findElement(By.xpath("//input[@id='password']"));
+
     }
     public WebElement getPasswordAgainField(){
         return driver.findElement(By.xpath("//input[@id='passwordAgain']"));
+
     }
     public WebElement getSignUpBtn(){
         return driver.findElement(By.xpath("//button[@id='signup']"));
+
     }
     public WebElement wrongEnteredEmail(){
         return driver.findElement(By.xpath("//div[@id='email-validation-tag']"));
+
+    }
+    public WebElement getMustMachString(){
+        return driver.findElement(By.xpath("//div[contains(@class,'validation-tag')]"));
+
     }
 
 }
