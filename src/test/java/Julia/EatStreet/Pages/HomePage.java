172ba83a91780d6c886439c111b499fb8df2aece
Yli Timoshenko <timoshkayli@gmail.com>
2021-11-16 15:49:33 +0200

    ForCustomers

--- src/test/java/Julia/EatStreet/Pages/HomePage.java
+++ src/test/java/Julia/EatStreet/Pages/HomePage.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
-
 public class HomePage extends BasePage {
 
     public HomePage(WebDriver driver) {
@@ -29,6 +28,22 @@ public class HomePage extends BasePage {
 
     public List<WebElement> getAllRest() {
         return driver.findElements(By.xpath("//ul[@id='all-rests']/li"));
+            }
+
+    public void getSelectPageRest() {
+        if (getPageNumberList().size() > 0) {
+            sleep(5);
+            getPageNumberList().get(getPageNumberList().size() - 1).click();
+        }
+    }
+
+    public void getSelect70Rest() {
+        if (getPageNumberList().size() > 0) {
+            getPageNumberList().get(1).click();
+            getAllRest().get(19).click();
+        } else {
+            getAllRest().get(getAllRest().size() - 1).click();
+        }
     }
 
     public List<WebElement> getSearchAttempt() {
@@ -36,7 +51,7 @@ public class HomePage extends BasePage {
     }
 
     public WebElement restName(){
-        return  findElement("//h1[contains(@class,'name')]");
+        return  findElement("//a[contains(text(), 'York City')]");
     }
 
     public WebElement clickSignInBtn() {
@@ -213,5 +228,9 @@ public class HomePage extends BasePage {
 
     public WebElement getAsianFood() {
         return driver.findElement(By.xpath("//span[contains(text(),'Bangladeshi')]"));
+    }
+
+    public WebElement getForCustomers() {
+        return driver.findElement(By.xpath("//a[contains(text(), 'For Customers')]"));
     }
 }
