d671d1d5e7e69c36c99244feccba5078c5ba97ff
Svetlana123411 <prutasveta@gmail.com>
2021-11-18 16:22:32 +0200

    Created new test checkLiveChatSendingRequest

--- src/test/java/Svetlana/EatstreetTest/LiveChatOpen.java
+++ src/test/java/Svetlana/EatstreetTest/LiveChatOpen.java
@@ -6,6 +6,8 @@ import Svetlana.EatstreetPage.SignInPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class LiveChatOpen extends TestInit {
     @Test
     public void liveChatOpen() {
@@ -38,6 +40,25 @@ public class LiveChatOpen extends TestInit {
         homePage.clickSendBtn().click();
         sleep(3);
         Assert.assertEquals(homePage.checkAnswerBotLiveChat().getText(), "inquiring about");
+    }
+    @Test
+    public void checkLiveChatSendingRequest(){
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        openUrl("https://qa2.eatstreet.com/");
+        HomePage homePage=new HomePage(driver);
+        homePage.getSignIn().click();
+        SignInPage signInPage = new SignInPage(driver);
+        signInPage.EnterEmail().sendKeys("prutasveta@gmail.com");
+        signInPage.EnterPassword().sendKeys("centik-1234");
+        signInPage.pressGoItBtn().click();
+        signInPage.pressSignInBtn();
+        homePage.getLiveChat().click();
+        homePage.goToTheSecondWindowTab(driver,10);
+        homePage.enterTextInTheLiveChat().sendKeys("Hello!");
+        homePage.clickSendBtn().click();
+        homePage.sendRequestLiveChat().click();
+        homePage.clickEndChatBtn().click();
+        Assert.assertTrue(homePage.checkEndChatBtn().isDisplayed());
     }
 }
 
