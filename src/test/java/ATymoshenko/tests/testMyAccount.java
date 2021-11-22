4d42e54788670055b3a7efa7fc3445566f96b42d
Alex <nika3akin@gmail.com>
2021-11-22 10:21:45 +0200

    added test for checking 1 Required Field in add a new address in my account

--- src/test/java/ATymoshenko/tests/testMyAccount.java
+++ src/test/java/ATymoshenko/tests/testMyAccount.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 
 public class testMyAccount extends TestInit {
     @Test
-    public void checkRequiredFields(){
+    public void checkRequiredFields0Pieces(){
         HomePage homePage = new HomePage(driver);
         openUrl("https://qa2.eatstreet.com/");
         homePage.getGoItBtn().click();
@@ -29,9 +29,26 @@ public class testMyAccount extends TestInit {
         sleep(2);
         Assert.assertTrue(homePage.getListRequired().size()==0);
     }
-//    @Test
-//    public void test1RequiredField(){
-//        HomePage homePage = new HomePage(driver);
-//        openUrl("https://qa2.eatstreet.com/");
-//    }
+    @Test
+    public void test1RequiredField1Pieces(){
+        HomePage homePage = new HomePage(driver);
+        openUrl("https://qa2.eatstreet.com/");
+        homePage.getGoItBtn().click();
+        homePage.addSignInBtnOnHeader().click();
+        homePage.addEmailField().sendKeys("alextymoshenko81@gmail.com");
+        homePage.addPasswordField().sendKeys("rterterteywyw");
+        homePage.addSignInBtnOnPopUpWindow().click();
+        sleep(2);
+        homePage.myAccountIcon().click();
+        sleep(2);
+        homePage.addNewAddressBtn().click();
+        homePage.addAptSuiteField().sendKeys("123");
+        homePage.addBuildingNameField().sendKeys("321");
+        homePage.addCityField().sendKeys("NY");
+        homePage.addStateField().sendKeys("New York");
+        homePage.addZipField().sendKeys("333");
+        homePage.addSaveBtnInAddANewAddressWindow().click();
+        sleep(2);
+        Assert.assertTrue(homePage.getListRequired().size()==1);
+    }
 }
