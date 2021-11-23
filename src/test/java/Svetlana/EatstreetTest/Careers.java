5c64b6c6d9b89ebab841d19fc2e3371ba0cf100b
Svetlana123411 <prutasveta@gmail.com>
2021-11-23 17:15:42 +0200

    Created new test careersGetDelivery

--- src/test/java/Svetlana/EatstreetTest/Careers.java
+++ src/test/java/Svetlana/EatstreetTest/Careers.java
@@ -10,22 +10,33 @@ import java.util.concurrent.TimeUnit;
 
 public class Careers extends TestInit {
     @Test
-    public void careers(){
+    public void careers() {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         openUrl("https://eatstreet.com/");
-        HomePage homePage=new HomePage(driver);
+        HomePage homePage = new HomePage(driver);
         homePage.getClickGoItBtn().click();
-        FooterPage footerPage=new FooterPage(driver);
+        FooterPage footerPage = new FooterPage(driver);
         footerPage.clickCareers().click();
         sleep(5);
         footerPage.inputSerchField().sendKeys("Director\n");
 
         Assert.assertTrue(footerPage.checkSerchField().isDisplayed());
+    }
 
-
-
-
-
-
+    @Test
+    public void careersGetDelivery() {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        openUrl("https://eatstreet.com/");
+        HomePage homePage = new HomePage(driver);
+        homePage.getClickGoItBtn().click();
+        FooterPage footerPage = new FooterPage(driver);
+        footerPage.clickCareers().click();
+        for (int i = 0; i < 4; i++) {
+            footerPage.getClickCategories().get(i).click();
+            sleep(2);
+        }
+        footerPage.getClickDelivery().click();
+        Assert.assertTrue(driver.getCurrentUrl().contains("https://eatstreet.com/driver-careers"));
     }
 }
+
