02ac00dc6304a0a73b623eede94672c0243746f2
Svetlana123411 <prutasveta@gmail.com>
2021-11-24 20:39:35 +0200

    +-

--- src/test/java/Svetlana/EatstreetTest/DriverApplyHere.java
+++ src/test/java/Svetlana/EatstreetTest/DriverApplyHere.java
@@ -1,12 +1,24 @@
 package Svetlana.EatstreetTest;
 
 import ClasesToAllUs.TestInit;
+import Svetlana.EatstreetPage.FooterPage;
+import Svetlana.EatstreetPage.HomePage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DriverApplyHere extends TestInit {
     @Test
     public void driverApplyHere(){
         openUrl("https://eatstreet.com/");
+        HomePage homePage=new HomePage(driver);
+        homePage.getClickGoItBtn().click();
+        FooterPage footerPage=new FooterPage(driver);
+        footerPage.clickDriver().click();
+        footerPage.getClickAddress().click();
+        footerPage.getClickCity().get(1).click();
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("https://eatstreet.com/careers/delivery-driver-ames-ia"));
+
 
     }
 }
