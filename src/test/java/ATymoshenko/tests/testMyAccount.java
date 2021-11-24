6c96a6e48b1afe78a852c77c89fe7944c1062e42
Alex <nika3akin@gmail.com>
2021-11-24 15:29:01 +0200

    added test about required 2 Messege in edit your info

--- src/test/java/ATymoshenko/tests/testMyAccount.java
+++ src/test/java/ATymoshenko/tests/testMyAccount.java
@@ -310,4 +310,27 @@ public class testMyAccount extends TestInit {
 
         Assert.assertTrue(homePage.addRequiredMessegeInChangeAccountInfoWindow().size()==3);
     }
+    @Test
+    public  void test2MessegeAboutRequiredInfoInEditYourInfoBtn(){
+        HomePage homePage = new HomePage(driver);
+        openUrl("https://eatstreet.com/");
+        homePage.getGoItBtn().click();
+        homePage.addSignInBtnOnHeader().click();
+        homePage.addEmailField().sendKeys("alextymoshenko81@gmail.com");
+        homePage.addPasswordField().sendKeys("rterterteywyw");
+        sleep(2);
+        homePage.addSignInBtnOnPopUpWindow().click();
+        sleep(2);
+        homePage.myAccountIcon().click();
+        sleep(2);
+        homePage.addEditYourInfoBtn().click();
+        homePage.addFirstNameFieldInChangeAccountInfoWindow().sendKeys("Clint");
+//        homePage.addLastNameFieldInChangeAccountInfoWindow().sendKeys("Eastwood");
+//        sleep(2);
+//        homePage.addPhoneNumberFieldInChangeAccountInfoWindow().sendKeys("+1 202 679 45 01");
+        homePage.addSaveBtnInChangeAccountInfoWindow().click();
+        sleep(2);
+
+        Assert.assertTrue(homePage.addRequiredMessegeInChangeAccountInfoWindow().size()==2);
+    }
 }
--- src/test/java/Julia/EatStreet/Pages/HomePage.java
+++ src/test/java/Julia/EatStreet/Pages/HomePage.java
@@ -339,7 +339,6 @@ public class HomePage extends BasePage {
         return driver.findElement(By.xpath("//h2[contains(text(),'All Jobs')]"));
     }
 
-<<<<<<< HEAD
     public WebElement clickDeliveryBtn() {
         return driver.findElement(By.xpath("//label[contains(text(),'Delivery')]"));
     }
@@ -347,14 +346,7 @@ public class HomePage extends BasePage {
     public WebElement clickTakeOutBtn() {
         return driver.findElement(By.xpath("//label[contains(text(),'Takeout')]"));
     }
-        public List<WebElement> getDesertLabel () {
-            return driver.findElements(By.xpath("//ul[@class='cuisine-types__list']//li"));
-        }
 
-        public WebElement getBaltimoreCity () {
-            return driver.findElement(By.xpath("//a[contains(text(),'Baltimore, MD')]"));
-        }
-=======
     public List<WebElement> getDesertLabel() {
         return driver.findElements(By.xpath("//ul[@class='cuisine-types__list']//li"));
     }
@@ -362,5 +354,4 @@ public class HomePage extends BasePage {
     public WebElement getBaltimoreCity() {
         return driver.findElement(By.xpath("//a[contains(text(),'Baltimore, MD')]"));
     }
->>>>>>> master
 }
