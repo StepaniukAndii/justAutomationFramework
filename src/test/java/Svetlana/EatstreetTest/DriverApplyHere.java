71314950be12f1fb1341e5d8f753085de6cf536a
Svetlana123411 <prutasveta@gmail.com>
2021-11-24 20:34:27 +0200

    Created new test driverApplyHere

---
+++ src/test/java/Svetlana/EatstreetTest/DriverApplyHere.java
@@ -0,0 +1,12 @@
+package Svetlana.EatstreetTest;
+
+import ClasesToAllUs.TestInit;
+import org.testng.annotations.Test;
+
+public class DriverApplyHere extends TestInit {
+    @Test
+    public void driverApplyHere(){
+        openUrl("https://eatstreet.com/");
+
+    }
+}
