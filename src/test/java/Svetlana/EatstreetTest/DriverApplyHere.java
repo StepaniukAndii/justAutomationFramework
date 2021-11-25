afc43cf7538b05868163d49409e3db00b69a6490
Svetlana123411 <prutasveta@gmail.com>
2021-11-25 10:28:29 +0200

    +-

--- src/test/java/Svetlana/EatstreetTest/DriverApplyHere.java
+++ src/test/java/Svetlana/EatstreetTest/DriverApplyHere.java
@@ -6,9 +6,13 @@ import Svetlana.EatstreetPage.HomePage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
 public class DriverApplyHere extends TestInit {
     @Test
     public void driverApplyHere(){
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         openUrl("https://eatstreet.com/");
         HomePage homePage=new HomePage(driver);
         homePage.getClickGoItBtn().click();
@@ -16,8 +20,19 @@ public class DriverApplyHere extends TestInit {
         footerPage.clickDriver().click();
         footerPage.getClickAddress().click();
         footerPage.getClickCity().get(1).click();
+        footerPage.clickApplyHere().get(0).click();
+        ArrayList<String> tabs2 = new ArrayList<String> (driver.getWindowHandles());
+        driver.switchTo().window(tabs2.get(1));
+        sleep(10);
+        footerPage.inputFerstName().sendKeys("Svetlana");
+        footerPage.inputLastName().sendKeys("Putas");
+        footerPage.inputEmail().sendKeys("putas@gmail.com");
+        footerPage.inputNumber().sendKeys("+152236552656");
+        sleep(5);
+//        footerPage.clickSubmit().click();
 
-        Assert.assertTrue(driver.getCurrentUrl().contains("https://eatstreet.com/careers/delivery-driver-ames-ia"));
+
+//        Assert.assertTrue(driver.getCurrentUrl().contains("https://eatstreet.com/careers/delivery-driver-ames-ia"));
 
 
     }
