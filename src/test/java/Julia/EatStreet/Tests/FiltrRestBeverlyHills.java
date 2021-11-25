4aaf80237152cbf9af47b73f10560e3d88f213ad
Yli Timoshenko <timoshkayli@gmail.com>
2021-11-25 21:11:02 +0200

    check Rest Beverly Hills California

--- src/test/java/Julia/EatStreet/Tests/FiltrRestBeverlyHills.java
+++ src/test/java/Julia/EatStreet/Tests/FiltrRestBeverlyHills.java
@@ -20,4 +20,25 @@ public class FiltrRestBeverlyHills extends TestInit {
         sleep(3);
         Assert.assertTrue(homePage.getBackBeverlyHills().isDisplayed());
     }
+
+    @Test
+    public void checkRestBeverlyHillsCalifornia() {
+        HomePage homePage = new  HomePage(driver);
+        openUrl("https://qa2.eatstreet.com/");
+        homePage.checkAndClousePopUpWindow();
+        homePage.getAdressInput().sendKeys("Beverly Hills");
+        sleep(5);
+        homePage.getFedBtn().click();
+        sleep(3);
+        homePage.getAllRest().get(3).click();
+        sleep(5);
+        homePage.sectionMenu().get(7).click();
+        sleep(3);
+        homePage.addThisItem().click();
+        homePage.addThisItem().click();
+        homePage.addThisItem().click();
+        homePage.addThisItem().click();
+        homePage.btnAddToCard().click();
+        Assert.assertTrue(homePage.ecqualsCard().getText().contains("5"));
+    }
 }
