a4f7a4df06a40c4423631e3a421794244a9cff95
Yli Timoshenko <timoshkayli@gmail.com>
2021-11-26 19:38:56 +0200

    Avtotest 35

--- src/test/java/Julia/EatStreet/Tests/FiltrRestBeverlyHills.java
+++ src/test/java/Julia/EatStreet/Tests/FiltrRestBeverlyHills.java
@@ -41,4 +41,44 @@ public class FiltrRestBeverlyHills extends TestInit {
         homePage.btnAddToCard().click();
         Assert.assertTrue(homePage.ecqualsCard().getText().contains("5"));
     }
+
+    @Test
+    public void deleteYourOrder(){
+        HomePage homePage = new  HomePage(driver);
+        openUrl("https://qa2.eatstreet.com/");
+        homePage.checkAndClousePopUpWindow();
+        homePage.getAdressInput().sendKeys("Beverly Hills");
+        sleep(5);
+        homePage.getFedBtn().click();
+        sleep(5);
+        homePage.getAllRest().get(3).click();
+        sleep(5);
+        homePage.sectionMenu().get(7).click();
+        sleep(3);
+        homePage.addThisItem().click();
+        sleep(5);
+        homePage.btnAddToCard().click();
+        sleep(5);
+        homePage.sectionMenu().get(9).click();
+        sleep(5);
+        homePage.addThisItem().click();
+        sleep(5);
+        homePage.btnAddToCard().click();
+        sleep(7);
+        homePage.sectionMenu().get(24).click();
+        sleep(5);
+        homePage.addThisItem().click();
+        sleep(5);
+        homePage.btnAddToCard().click();
+        homePage.deleteItemInYourOrder().get(4).click();
+        sleep(10);
+        homePage.deleteItemInYourOrder().get(6).click();
+        sleep(10);
+        homePage.deleteItemInYourOrder().get(10).click();
+        sleep(10);
+        //homePage.deleteItemInYourOrder().get(14).click();
+       // sleep(7);
+        Assert.assertTrue(homePage.ecqualsCard().getText().contains("4"));
+    }
 }
+
