334d5a02149bbfc891c57d718a9c791b430fe295
NikitaShushevich <nikita.shushevich@gmail.com>
2021-11-26 14:47:28 +0200

    added new method in TestInit, add written FilterTest

---
+++ src/test/java/TestNikita/GlobusTop/FilterTest.java
@@ -0,0 +1,20 @@
+package TestNikita.GlobusTop;
+
+import ClasesToAllUs.TestInit;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FilterTest extends TestInit {
+    @Test
+    public void testFilter() {
+        GlobusPage homePage = new GlobusPage(driver);
+        openUrl("https://globus.top/uk");
+        homePage.closedFilter().click();
+        homePage.insideFilterElemComp().click();
+        Assert.assertEquals(getTextFromXpath(homePage.getElemTextOnPage()), "Комп’ютери та периферія");
+        homePage.clickLogoToReturn().click();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://globus.top/uk");
+    }
+}
+
+
