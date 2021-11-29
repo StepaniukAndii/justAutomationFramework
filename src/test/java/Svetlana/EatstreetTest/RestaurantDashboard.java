242d148d775357fdced091f75cc71d76bd96d0c3
Svetlana123411 <prutasveta@gmail.com>
2021-11-29 15:42:25 +0200

    Created new Test restaurantDashboardOpene

--- src/test/java/Svetlana/EatstreetTest/RestaurantDashboard.java
+++ src/test/java/Svetlana/EatstreetTest/RestaurantDashboard.java
@@ -1,11 +1,35 @@
 package Svetlana.EatstreetTest;
 
 import ClasesToAllUs.TestInit;
+import Svetlana.EatstreetPage.FooterPage;
+import Svetlana.EatstreetPage.HomePage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class RestaurantDashboard extends TestInit {
     @Test
     public void  restaurantDashboardOpened(){
-        openUrl("Eatsteet");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        openUrl("https://eatstreet.com/");
+        HomePage homePage=new HomePage(driver);
+        homePage.getClickGoItBtn().click();
+        FooterPage footerPage=new FooterPage(driver);
+        footerPage.clickRestaurantDashboard().click();
+        footerPage.inputEmailSignIn().sendKeys("prutasveta@gmail.com");
+        footerPage.inputPasswordSignIn().sendKeys("12598645221552");
+        footerPage.clickSignIn().click();
+        sleep(3);
+        footerPage.clickForgotPassword().click();
+        footerPage.inputNewPassword().sendKeys("00000000");
+        footerPage.clickResetPassword().click();
+
+        Assert.assertTrue(footerPage.checkResetPassword().isDisplayed());
+
+
+
+
+
     }
 }
