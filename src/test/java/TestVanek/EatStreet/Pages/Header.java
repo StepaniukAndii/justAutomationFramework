db98e4f8d09be0dd60b24186d7fa0a6b71906877
Vanecheek <74904746+Vanecheek@users.noreply.github.com>
2021-11-30 14:02:25 +0200

    create new test on back to home screen

---
+++ src/test/java/TestVanek/EatStreet/Pages/Header.java
@@ -0,0 +1,14 @@
+package TestVanek.EatStreet.Pages;
+
+import ClasesToAllUs.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Header extends BasePage {
+    public Header(WebDriver driver) {
+        super(driver);
+    }
+    public WebElement logoBtn(){
+        return findElementByXpath("//a[contains(@class,'logo')]");
+    }
+}
