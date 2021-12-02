bed09c1aacb0d8552d976a72f9c6371e5c4fa402
Yli Timoshenko <timoshkayli@gmail.com>
2021-12-02 09:29:44 +0200

    Take Out Rest In City

--- src/test/java/Julia/EatStreet/Tests/DeliveryTakeOutBtn.java
+++ src/test/java/Julia/EatStreet/Tests/DeliveryTakeOutBtn.java
@@ -17,4 +17,16 @@ public class DeliveryTakeOutBtn extends TestInit {
         homePage.getFedBtn().click();
         Assert.assertTrue(homePage.getAdressSearchAttempt().get(0).isDisplayed());
     }
+
+    @Test
+    public void TakeOutBtn(){
+        HomePage homePage = new HomePage(driver);
+        openUrl("https://qa2.eatstreet.com/");
+        homePage.checkAndClousePopUpWindow();
+        homePage.getAdressInput().sendKeys("San Francisco, CA");
+        homePage.clickTakeOutBtn().click();
+        homePage.getFedBtn().click();
+        sleep(5);
+        Assert.assertTrue(homePage.getRadioBtnTakeOut().isDisplayed());
+    }
 }
