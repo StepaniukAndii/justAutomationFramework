d1981e917634e61925e3cc659d72122ebaf1362d
Vanecheek <74904746+Vanecheek@users.noreply.github.com>
2021-12-02 16:41:35 +0200

    test start page media link Instagram

---
+++ src/test/java/TestVanek/EatStreet/Tests/TestInstagramLink.java
@@ -0,0 +1,25 @@
+package TestVanek.EatStreet.Tests;
+
+import ClasesToAllUs.TestInit;
+import TestVanek.EatStreet.Pages.StartPage;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+
+public class TestInstagramLink extends TestInit {
+    StartPage startPage;
+    @Test
+    public void testInstagramLink(){
+        startPage = new StartPage(driver);
+        openUrl("https://eatstreet.com/");
+        startPage.gotItBtn().click();
+        startPage.mediaLinks().get(2).click();
+        ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1));
+        Boolean wait = new WebDriverWait(driver,10).until(ExpectedConditions.urlContains("instagram.com/eatstreethq/"));
+        Assert.assertTrue(driver.getCurrentUrl().contains("instagram.com/eatstreethq/"));
+    }
+}
