758881bc3289f5b4dcbc4f3e87cd40b6f09a3a54
Vanecheek <74904746+Vanecheek@users.noreply.github.com>
2021-12-02 16:04:34 +0200

    test on appear cart from start page

--- src/test/java/TestVanek/EatStreet/Pages/Header.java
+++ src/test/java/TestVanek/EatStreet/Pages/Header.java
@@ -11,4 +11,10 @@ public class Header extends BasePage {
     public WebElement logoBtn(){
         return findElementByXpath("//a[contains(@class,'logo')]");
     }
+    public WebElement cart(){
+        return findElementByXpath("//div[contains(@ng-click,'cart')]");
+    }
+    public WebElement cartOptionBtn(){
+        return findElementByXpath("//a[contains(@class,'btn--orange')]");
+    }
 }
