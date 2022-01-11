9a31c6495eb87f75d92fcc6538be4c12d1645d7b
Alex <nika3akin@gmail.com>
2022-01-11 10:03:15 +0200

    add checking My account btn on header

---
+++ src/test/java/ATymoshenko/tests/testsSignUp.java
@@ -0,0 +1,23 @@
+package ATymoshenko.tests;
+
+import ATymoshenko.pages.HomePage;
+import ClasesToAllUs.TestInit;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class testsSignUp extends TestInit {
+    @Test
+    public void checkMyAccountBtn(){
+        HomePage homePage = new HomePage(driver);
+        openUrl("https://qa2.eatstreet.com/");
+        homePage.getGoItBtn().click();
+        homePage.addSignInBtnOnHeader().click();
+        homePage.addEmailField().sendKeys("alextymoshenko81@gmail.com");
+        homePage.addPasswordField().sendKeys("rterterteywyw");
+        sleep(2);
+        homePage.addSignInBtnOnPopUpWindow().click();
+        sleep(2);
+
+        Assert.assertTrue(homePage.myAccountIcon().isDisplayed());
+    }
+}
