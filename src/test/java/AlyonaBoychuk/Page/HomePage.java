9e69766a632aef1e7e6fd88afd8ee45c7f9ab7c4
LeraDalya <alyona.boychuk@gmail.com>
2022-01-14 16:31:04 +0200

    Create Autotest for AboutUS

---
+++ src/test/java/AlyonaBoychuk/Page/HomePage.java
@@ -0,0 +1,25 @@
+package AlyonaBoychuk.Page;
+
+import ClasesToAllUs.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends BasePage {
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+    public void checkAboutUs() {driver.findElement(By.xpath("//a [@ui-sref='about-us']")).click();
+    }
+
+    public void clickGetTheAppLink() {
+        driver.findElement(By.xpath("//a[text()='Get The App']")).click();
+    }
+
+    public void clouseGoItbutton() {
+        driver.findElement(By.xpath("//button[@class='btn']")).click();
+    }
+
+    public boolean getUrlAboutUsIsDisplayed(){
+        return driver.getCurrentUrl().contains("about-us");
+    }
+}
