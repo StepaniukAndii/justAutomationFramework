27ea422ca8f2a2816167c48dd27628ccb77e00a3
LeraDalya <alyona.boychuk@gmail.com>
2022-01-18 11:14:38 +0200

    Best Line Kabinet

---
+++ src/test/java/AlyonaBoychuk/Test/PulReqwest.java
@@ -0,0 +1,18 @@
+package AlyonaBoychuk.Test;
+import AlyonaBoychuk.Page.HomePage;
+import ClasesToAllUs.TestInit;
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+public class PulReqwest extends TestInit {
+    @Test
+    public void checkBestLineKabinet() {
+        HomePage homePage = new HomePage(driver);
+        openUrl("http://bestline.net.ua/");
+        homePage.getButtonKabinet().click();
+        Assert.assertTrue(homePage.isDisplayedPlatagi());
+        Assert.assertTrue(homePage.isDisplayedPopovnytyRahunok());
+        Assert.assertTrue(homePage.isDisplayedTymchasovyyPlatig());
+    }
+
+}
