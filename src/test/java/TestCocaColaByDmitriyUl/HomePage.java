0b07fac68ab1710900896633e52fb9ec867c52a2
DmitriyUl <uliatovskiy@gmail.com>
2022-01-24 15:26:41 +0200

    TestCocaCola

--- src/test/java/TestCocaColaByDmitriyUl/HomePage.java
+++ src/test/java/TestCocaColaByDmitriyUl/HomePage.java
@@ -9,27 +9,5 @@ public class HomePage extends TestInit{
         closeCookie();
         checkResult();
     }
-    @Test
-    public void navBar(){
-        navigateHomePage();
-        closeCookie();
-        navigateBar();
-        checkResultNavBar();
-    }
-    @Test
-    public void logoTest(){
-        navigateHomePage();
-        closeCookie();
-        navigateBar();
-        clickOurBrands();
-        clickLogo();
-        checkResultLogo();
-    }
-    @Test
-    public void ourBrands() {
-        navigateHomePage();
-        closeCookie();
-        navigateBar();
-        clickOurBrands();
-    }
+
 }
