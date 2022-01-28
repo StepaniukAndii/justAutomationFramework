69b529ba5124b4f212421cdf313aa712ce9c8a1f
dvpogoreliy <98585108+dvpogoreliy@users.noreply.github.com>
2022-01-28 14:23:11 +0200

    Streambutton SoundCloud

--- src/test/java/TestDenchik/SoundCloud/Tests/Streambutton.java
+++ src/test/java/TestDenchik/SoundCloud/Tests/Streambutton.java
@@ -7,13 +7,13 @@ import org.testng.annotations.Test;
 
 public class Streambutton extends TestInit {
     LandingPageSoundCloud landingPageSoundCloud;
-        @Test
-        public void Streambutton(){
-            driver.get("https://soundcloud.com/discover");
-            landingPageSoundCloud = new LandingPageSoundCloud(driver);
-            landingPageSoundCloud.cookies().click();
-            landingPageSoundCloud.streamButton().click();
-            Assert.assertTrue(driver.getCurrentUrl().contains("stream"));
+    @Test
+    public void Streambutton(){
+        driver.get("https://soundcloud.com/discover");
+        landingPageSoundCloud = new LandingPageSoundCloud(driver);
+        landingPageSoundCloud.cookies().click();
+        landingPageSoundCloud.streamButton().click();
+        Assert.assertTrue(driver.getCurrentUrl().contains("stream"));
 
 
     }
