708546e7bdb09cfb1b432c0f26ca53ab6e5bc471
Immortal9113 <yuriy.yermakov02@gmail.com>
2022-01-31 13:58:00 +0300

    OrderValidCheckLoraShen git commit -m OrderValidCheckLoraShen

--- src/test/java/TestYurchik/LoraShen/Pages/CartPageLoraShen.java
+++ src/test/java/TestYurchik/LoraShen/Pages/CartPageLoraShen.java
@@ -12,11 +12,11 @@ public class CartPageLoraShen extends BasePage {
     public WebElement deleteZeroElement(){ return findElementByXpath("//a[@data-marker = '#id_items-0']"); }
     public List<WebElement> checkCartEmpty() { return findElementsByXpath("//div[@class = 'cart-slot__empty']"); }
     public List<WebElement> checkCartHasElement() { return findElementsByXpath("//span[contains(text(), '4412')]"); }
-    public WebElement cartNameLine() { return findElementByXpath("//input [@id = 'id_billing_detail_last_first_name']"); }
+    public WebElement cartNameLine() { return findElementByXpath("//input [contains(@id, 'id_billing_detail_l')]"); }
     public WebElement cartEmailLine() { return findElementByXpath("//input[@id = 'id_billing_detail_email']"); }
     public WebElement cartMobilePhone() { return findElementByXpath("//input[@id = 'id_billing_detail_phone']"); }
-    public WebElement cartMeBuyerCheckBox() { return findElementByXpath("//label[@for = 'id_same_billing_shipping']"); }
-    public WebElement cartSelectTime() { return findElementByXpath("//label[@for = 'time-self-6']"); }
+    public WebElement cartMeBuyerCheckBox() { return findElementByXpath("//label[contains(@for, 'id_same')]"); }
+    public WebElement cartSelectTime() { return findElementByXpath("//label[contains(@for, 'self-6')]"); }
     public WebElement cartCompleteOrder() { return findElementByXpath("//input[@name= 'complete_order'][@type = 'submit']"); }
-    public WebElement cartSelectAdress() { return findElementByXpath("//label[@for = 'addr1']"); }
+    public WebElement cartSelectAdress() { return findElementByXpath("//label[contains(@for, 'addr1')]"); }
 }
