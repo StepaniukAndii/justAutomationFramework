f4e3021c529f71ffb9fca43ecec0f8c5d0a4340e
LazarchukMark <lazarchuk.mark.08@gmail.com>
2022-02-01 16:48:49 +0300

    ApplyInYourCityDeliveyDriver

--- src/test/java/TestMarkEatStreet/Test/PartnerWithUsTest.java
+++ src/test/java/TestMarkEatStreet/Test/PartnerWithUsTest.java
@@ -23,9 +23,7 @@ public class PartnerWithUsTest extends TestInit {
     body.applybtn().click();
     driverPage.applyinyourcitybtn().click();
     driverPage.deliverydriveramesbtn().click();
-     Boolean wait = new WebDriverWait(driver, 10).
-             until(ExpectedConditions.urlToBe("https://qa2.eatstreet.com/careers/delivery-driver-ames-ia"));
-     Assert.assertEquals(driver.getCurrentUrl(), "https://qa2.eatstreet.com/careers/delivery-driver-ames-ia");
+     Assert.assertTrue(driver.getCurrentUrl().contains("ames-ia"));
  }
 
 }
