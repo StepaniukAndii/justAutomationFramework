2670544cc51d9bda915ee72f4cf544e5458dde90
YevtushenkoVova <yevtushenko.vladimir86@gmail.com>
2022-02-03 15:46:27 +0200

    LegalTests

--- src/test/java/YevtushenkoVova/Pages/HomePage.java
+++ src/test/java/YevtushenkoVova/Pages/HomePage.java
@@ -7,33 +7,59 @@ import org.openqa.selenium.WebElement;
 
 public class HomePage extends BasePage {
 
-    public HomePage(WebDriver driver){
-        super (driver);
+    public HomePage(WebDriver driver) {
+        super(driver);
     }
+
     public WebElement buttonGotIt() {
         return findElement("//button[@class='btn']");
     }
-    public WebElement openPageSingIn(){
+
+    public WebElement openPageSingIn() {
         return findElement("//a[@id='menu-signin']");
     }
 
-    public WebElement getMyAccound(){
+    public WebElement getMyAccound() {
         return driver.findElement(By.xpath("//a[@id='menu-my-account']"));
     }
-    public void openQa2HomePage (){
+
+    public void openQa2HomePage() {
         driver.get("https://qa2.eatstreet.com");
     }
-    public WebElement aboutUs(){
+
+    public WebElement aboutUs() {
         return driver.findElement(By.xpath("//a[@ui-sref='about-us']"));
     }
-    public WebElement contactUs(){
+
+    public WebElement contactUs() {
         return driver.findElement(By.xpath("//a[@ui-sref='contact']"));
     }
-    public WebElement careers(){
+
+    public WebElement careers() {
         return driver.findElement(By.xpath("//a[@ui-sref='careers']"));
     }
-    public WebElement blog(){
+
+    public WebElement blog() {
         return driver.findElement(By.xpath("//a[@href='http://blog.eatstreet.com/']"));
+    }
+
+    public WebElement linkPrivacyPolicy() {
+        return driver.findElement(By.xpath("//a[@ui-sref='privacy']"));
+    }
+    public WebElement privacyText(){
+        return driver.findElement(By.xpath("//h2[contains(text(),'Privacy Policy')]"));
+    }
+    public WebElement linkTermsofUse(){
+        return driver.findElement(By.xpath("//a[@href='/terms']"));
+    }
+    public WebElement termsText(){
+        return driver.findElement(By.xpath("//h2[contains(text(),'Terms of Use')]"));
+    }
+    public WebElement linkRestaurantTerms(){
+        return driver.findElement(By.xpath("//a[@ui-sref='restaurant-terms-of-service']"));
+    }
+    public WebElement merchantTerms(){
+        return driver.findElement(By.xpath("//h2[contains(text(),'Merchant Terms')]"));
     }
 }
 
