5e886f70e8a91341a09346087ade379afd6110b1
Serg <kohan_sergey@ukr.net>
2022-02-09 17:20:30 +0200

    Test dodavannya tovara z Novinki

---
+++ src/test/java/SergKokan/Test/TestFooter.java
@@ -0,0 +1,24 @@
+package SergKokan.Test;
+
+import ClasesToAllUs.TestInit;
+import SergKokan.Page.*;
+import SergKokan.Page.HeaderLinkPPage.Novinki;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestFooter extends TestInit {
+    @Test
+    public void FooterNovinki(){
+        HomePage homePage= new HomePage(driver);
+        Novinki novinki = new Novinki(driver);
+        VyborTovaraPage vyborTovara = new VyborTovaraPage(driver);
+        DobavitVKorzinu dobavitVKorzinu = new DobavitVKorzinu(driver);
+    //    VyborTovaraPage vyborTovara = new VyborTovaraPage(driver);
+        homePage.navigatePrego();
+        novinki.btnNovinki().click();
+        vyborTovara.btnVyborTovara().click();
+        dobavitVKorzinu.clickKupyty().click();
+        Assert.assertTrue(dobavitVKorzinu.tovarVKorzine().isDisplayed());
+
+    }
+}
