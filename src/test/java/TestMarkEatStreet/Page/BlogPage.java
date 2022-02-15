a0234a0edbcc1ffc1e3749f6f9239d9d10f924e5
LazarchukMark <lazarchuk.mark.08@gmail.com>
2022-02-15 12:00:04 +0300

    CheckLGBTQTextEatStreetTestForMark

--- src/test/java/TestMarkEatStreet/Page/BlogPage.java
+++ src/test/java/TestMarkEatStreet/Page/BlogPage.java
@@ -7,4 +7,5 @@ import org.openqa.selenium.WebElement;
 public class BlogPage extends BasePage{
     public BlogPage(WebDriver driver){super(driver);}
     public WebElement enneagramtextbtn(){return findElementByXpath("//a[contains(@title,'Permalink to What')]");}
+    public WebElement lgbtqtextbtn(){return findElementByXpath("//a[contains(@title,'Permalink to LGBTQ')]");}
 }
