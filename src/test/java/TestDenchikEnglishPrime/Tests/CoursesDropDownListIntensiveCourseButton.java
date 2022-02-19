2c6bac332fc87c5b9e4e96c5531d704b294e8c02
dvpogoreliy <98585108+dvpogoreliy@users.noreply.github.com>
2022-02-19 20:06:24 +0200

    Courses Drop Down List Intensive Course Button English Prime

---
+++ src/test/java/TestDenchikEnglishPrime/Tests/CoursesDropDownListIntensiveCourseButton.java
@@ -0,0 +1,23 @@
+package TestDenchikEnglishPrime.Tests;
+
+import ClasesToAllUs.TestInit;
+import TestDenchikEnglishPrime.Pages.LandingPageEnglishPrime;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CoursesDropDownListIntensiveCourseButton extends TestInit {
+    LandingPageEnglishPrime landingPageEnglishPrime;
+    @Test
+    public void coursesDropDownListIntensiveCourseButton(){
+        driver.get("https://englishprime.ua/");
+        landingPageEnglishPrime = new LandingPageEnglishPrime(driver);
+        WebElement element = landingPageEnglishPrime.navMenuCoursesButton();
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element).perform();
+        landingPageEnglishPrime.coursesDropDownListIntensiveCourseButton().click();
+        Assert.assertTrue(driver.getCurrentUrl().contains("intensiv"));
+    }
+}
