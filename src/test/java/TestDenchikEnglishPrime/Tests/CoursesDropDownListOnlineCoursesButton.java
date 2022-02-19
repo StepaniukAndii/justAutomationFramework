f2078ff383ef3ccde9c576feb69cd4371130e0a5
dvpogoreliy <98585108+dvpogoreliy@users.noreply.github.com>
2022-02-19 19:27:13 +0200

    Courses Drop Down List Online Courses Button English Prime

---
+++ src/test/java/TestDenchikEnglishPrime/Tests/CoursesDropDownListOnlineCoursesButton.java
@@ -0,0 +1,23 @@
+package TestDenchikEnglishPrime.Tests;
+
+import ClasesToAllUs.TestInit;
+import TestDenchikEnglishPrime.Pages.LandingPageEnglishPrime;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CoursesDropDownListOnlineCoursesButton extends TestInit {
+    LandingPageEnglishPrime landingPageEnglishPrime;
+    @Test
+    public void coursesDropDownListOnlineCoursesButton(){
+        driver.get("https://englishprime.ua/");
+        landingPageEnglishPrime = new LandingPageEnglishPrime(driver);
+        WebElement element = landingPageEnglishPrime.navMenuCoursesButton();
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element).perform();
+        landingPageEnglishPrime.coursesDropDownListOnlineCoursesButton().click();
+        Assert.assertTrue(driver.getCurrentUrl().contains("onlajn-kursy-anglijskogo-yazyka"));
+    }
+}
