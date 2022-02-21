d94a5f060624a389da83cb29f246744b21b7a725
LazarchukMark <lazarchuk.mark.08@gmail.com>
2022-02-21 11:01:32 +0300

    TwitterLinkTestForMark

--- src/test/java/TestMarkEatStreet/Page/Footer.java
+++ src/test/java/TestMarkEatStreet/Page/Footer.java
@@ -21,4 +21,5 @@ public class Footer extends BasePage{
     public WebElement googleplaybtn(){return findElementByXpath("//a[contains(@class,'app-btn-android')]");}
     public WebElement appstorebtn(){return findElementByXpath("//a[contains(@class,'app-btn-iphone')]");}
     public WebElement facebookbtn(){return findElementByXpath("//a[contains(@href,'https://www.facebook')]");}
+    public WebElement twitterbtn(){return findElementByXpath("//a[contains(@href,'https://twitter')]");}
 }
